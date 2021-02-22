9451c957f8bd17efa67184af3f73ad5ee6b4c8c7
CS <43161373+csnewcs@users.noreply.github.com>
2021-02-22 10:27:05 +0900

    Commit in school

--- structs.cs
+++ structs.cs
@@ -17,10 +17,24 @@ namespace RaindropsCustomAssist
     struct Event
     {
         double _time;
+        double _yJudge:
+        double _speed;
+        double _color;
+
+        public Event(double time, double yJudge, double speed, double color)
+        {
+            _time = time;
+            _yJudge = yJudge;
+            _speed = speed;
+            _color = color;
+        }
     }
     struct Note
     {
         double _time;
+        double _xPoint:
+        double _length;
+        double _endXPoint;
         NoteType _type;
         public double time
         {
@@ -30,10 +44,21 @@ namespace RaindropsCustomAssist
         {
             get {return _type;}
         }
-        public Note(double time, NoteType noteType)
+        public Note(double time, NoteType noteType, double xPoint, double length, double endXPoint)
         {
             _time = time;
             _type = noteType;
+            _xPoint = xPoint;
+            _length = length;
+            _endXPoint = endXPoint;
+        }
+        public Note(double time, NoteType noteType, double xPoint)
+        {
+            _time = time;
+            _type = noteType;
+            _xPoint = xPoint;
+            _length = 0;
+            _endXPoint = xPoint;
         }
     }
     enum NoteType
