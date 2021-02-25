9b199bacdaee100b99e3f3984323c44dda96d353
csnewcs <cs.newcs0@gmail.com>
2021-02-25 18:46:41 +0900

    OpenFile

--- Main.cs
+++ Main.cs
@@ -10,27 +10,21 @@ namespace RaindropsCustomAssist
 {
     partial class MainPage
     {
-        private void OpenMusicClicked(object o, EventArgs e)
+        string musicPath = "";
+        string jsonPath = "";
+        private void openMusicButton_opened(object o,  EventArgs e)
         {
-            FileChooserDialog fileChooser = new FileChooserDialog("음악 파일(.mp3 혹은.wav)를 선택하세요", null, FileChooserAction.Open, "불러오기", ResponseType.Accept);
-            FileFilter musicFileFilter = new FileFilter(); musicFileFilter.AddPattern("*.mp3"); musicFileFilter.AddPattern("*.wav");
-            fileChooser.Filter = musicFileFilter;
-            fileChooser.Run();
-            string fileName = fileChooser.Filename;
-            Console.WriteLine(fileName);
-            musicPath.Text = fileName;
-            fileChooser.Dispose();
+            string filename = openMusicButton.Filename;
+            if (filename == "") return;
+            musicPath = filename;
+            if(jsonPath != "") doneButton.Sensitive = true;
         }
-        private void OpenJsonClicked(object o, EventArgs e)
+        private void openJsonButton_opened(object o, EventArgs e)
         {
-            FileChooserDialog fileChooser = new FileChooserDialog("채보 파일(.json)을 선택하세요", null, FileChooserAction.Open, "불러오기", ResponseType.Accept);
-            FileFilter jsonFileFilter = new FileFilter(); jsonFileFilter.AddPattern("*.json");
-            fileChooser.Filter = jsonFileFilter;
-            fileChooser.Run();
-            string fileName = fileChooser.Filename;
-            Console.WriteLine(fileName);
-            jsonPath.Text = fileName;
-            fileChooser.Dispose();
+            string filename = openJsonButton.Filename;
+            if (filename == "") return;
+            jsonPath = filename;
+            if(musicPath != "") doneButton.Sensitive = true;
         }
     }
 }
--- Main.gtk.cs
+++ Main.gtk.cs
@@ -12,12 +12,15 @@ namespace RaindropsCustomAssist
 {
     partial class MainPage : Gtk.Window
     {
-        Entry musicPath = new Entry();
-        Entry jsonPath = new Entry();
+        Button doneButton = new Button("시작");
+        FileChooserButton openMusicButton = new FileChooserButton("음악 파일 열기", FileChooserAction.Open);
+        FileChooserButton openJsonButton = new FileChooserButton("Json 파일 열기", FileChooserAction.Open);
+
         public MainPage() : base("방울비 커스텀 보조 프로그램")
         {
             DeleteEvent += delegate {Application.Quit();};
-            SetDefaultSize(300, 180);
+            SetDefaultSize(350, 220);
+            Resizable = false;
 
             Grid mainGrid = new Grid(); //모든 위젯들을 담는 메인 표
             Grid firstGrid = getFirstGrid(); //프로그램 켰을 때 뜨는 표, 곡 선택 등
@@ -31,30 +34,40 @@ namespace RaindropsCustomAssist
         private Grid getFirstGrid()
         {
             Grid grid = new Grid();
-            FileChooserButton openMusicButton = new FileChooserButton("음악 파일 열기", FileChooserAction.Open);
-            openMusicButton.Halign = Align.Center;
-            openMusicButton.Valign = Align.Center;
-            FileChooserButton openJsonButton = new FileChooserButton("Json 파일 열기", FileChooserAction.Open);
-            openJsonButton.Halign = Align.Center;
-            openJsonButton.Valign = Align.Center;
+            Label openMusicLable = new Label("음악 파일 열기");
+            Label openJsonLable = new Label("JSON 파일 열기");
+            doneButton.Sensitive = false;
+
+            openMusicButton.FileSet += openMusicButton_opened;
+            openJsonButton.FileSet += openJsonButton_opened;
+            
             FileFilter musicFileFilter = new FileFilter(); musicFileFilter.AddPattern("*.mp3"); musicFileFilter.AddPattern("*.wav");
             FileFilter jsonFileFilter = new FileFilter(); jsonFileFilter.AddPattern("*.json");
             openMusicButton.AddFilter(musicFileFilter);
             openJsonButton.AddFilter(jsonFileFilter);
 
-            grid.Attach(openMusicButton, 1, 1, 1, 1);
-            grid.Attach(openJsonButton, 1, 2, 1, 1);
+            grid.Attach(openMusicLable, 1, 1, 1, 1);
+            grid.Attach(openJsonLable, 1, 2, 1,1);
+            grid.Attach(openMusicButton, 2, 1, 1, 1);
+            grid.Attach(openJsonButton, 2, 2, 1, 1);
+            grid.Attach(doneButton, 3, 1, 1, 2);
             grid.RowHomogeneous = true;
             grid.ColumnHomogeneous = true;
+            grid.RowSpacing = 15;
+            grid.ColumnSpacing = 15;
+            grid.Margin = 20;
 
-            // Button openMusic = new Button("음악 파일 열기");
-            // Button openJson = new Button("채보 파일 열기");
-            // openMusic.Clicked += OpenMusicClicked;
-            // openJson.Clicked += OpenJsonClicked;
-            // grid.Attach(musicPath, 1, 1, 2, 1);
-            // grid.Attach(openMusic, 3, 1, 1, 1);
-            // grid.Attach(jsonPath, 1, 2, 2, 1);
-            // grid.Attach(openJson, 3, 2, 1, 1);
+            //CSS를 위한 이름 및 클래스 설정
+            openMusicLable.Name = "OpenMusicLable";
+            openMusicLable.StyleContext.AddClass("OpenFileLable");
+            openMusicButton.Name = "OpenMusicButton";
+            openMusicButton.StyleContext.AddClass("OpenFileButton");
+            openJsonLable.Name = "OpenJsonLable";
+            openJsonLable.StyleContext.AddClass("OpenFileLable");
+            openJsonButton.Name = "OpenJsonButton";
+            openJsonButton.StyleContext.AddClass("OpenFileButton");
+            doneButton.Name = "DoneButton";
+
             return grid;
         }
     }
